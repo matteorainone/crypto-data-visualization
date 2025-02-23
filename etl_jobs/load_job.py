ec25ebfb0d854ccbd3276eea3e66d2e8c4c3a6cb
matteorainone <matteo.rainone97@gmail.com>
2025-02-23 17:41:35 +0100

    load_job modifiche conclusive

--- etl_jobs/load_job.py
+++ etl_jobs/load_job.py
@@ -1,51 +1,58 @@
 import sys
-import pandas as pd
-import numpy as np
-import boto3
+import redshift_connector
 from awsglue.utils import getResolvedOptions
-from io import BytesIO
 
 args = getResolvedOptions(sys.argv, 
                           ['JobName',
                            'BucketName',
                            'SourcePath',
+                           'Crypto',
+                           'IamRole',
                            'RedshiftCluster',
-                           'RedshiftDatabase', 
-                           'DbSchema', 
-                           'DbUser', 
+                           'RedshiftDatabase',
+                           'DbSchema',
+                           'DbUser',
                            'DbPwd',
                            'DbPort'])
 
 job_name = args['JobName']
 bucket_name = args['BucketName']
 source = args['SourcePath']
-target = args['TargetPath']
+iam_role = args['IamRole']
 crypto = args['Crypto']
-catalog_db = args['DataCatalogDb']
-
-prices = args['PricesDataset']
-trend = args['TrendDataset']
+cluster = args['RedshiftCluster']
+db = args['RedshiftDatabase']
+db_schema = args['DbSchema']
+db_user = args['DbUser']
+db_pwd = args['DbPwd']
+db_port = int(args['DbPort'])
 
 #definizione della query per la creazione della tabella di destinazione
 create_table_sql = f"""
-CREATE TABLE IF NOT EXISTS crypto.{crypto} (
+CREATE TABLE IF NOT EXISTS {db_schema}.{crypto} (
     Date DATE,
-    Price DECIMAL(10,2),
-    Google_trend_index INT
-    )"""
+    Price FLOAT,
+    Google_trend_index BIGINT
+    );
+    """
 
 #definizione della query per l'inserimento dei dati in tabella
 insert_values_sql = f"""
-INSERT INTO crypto.{crypto} (Date, Price, Google_trend_index) VALUES
-()
+COPY {db_schema}.{crypto}
+FROM 's3://{bucket_name}/{source}{crypto}.parquet'
+IAM_ROLE '{iam_role}'
+FORMAT AS PARQUET;
 """
 
-redshift_client = boto3.client('redshift-data')
-redshift_client.excecute_statement(
-    ClusterIdentifier='', #cluster
-    Database='', #nome_db
-    DbUser='', #utenza db
-    sql="""""" #comando sql da eseguire
-)
+#inizializzazione della connessione al datawarehouse
+with redshift_connector.connect(host=cluster, database=db, user=db_user, 
+                                password=db_pwd, port=db_port) as conn:
+    with conn.cursor() as cursor:
+        #creazione della tabella
+        cursor.execute(create_table_sql)
 
-#VERIFICARE SU DOC UFFICIALE LA GESTIONE DEL METODO
+        #inserimento dei dati
+        cursor.execute(insert_values_sql)
+
+        #inserimento del commit
+        conn.commit()
